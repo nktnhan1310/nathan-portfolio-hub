2d980d60b4275b388d4b6b346b06e00d6a23e3bf
NKTNHAN\Admin <nhannkt1995@gmail.com>
2025-07-01 19:37:05 +0700

    Update linkedin url

--- frontend/src/components/ContactSection.tsx
+++ frontend/src/components/ContactSection.tsx
@@ -23,7 +23,7 @@ const ContactSection = () => {
             variant="secondary" 
             size="lg"
             className="bg-white text-blue-600 hover:bg-blue-50 hover:scale-105 transition-all duration-200 w-full sm:w-auto"
-            onClick={() => window.open('https://linkedin.com/in/nguyen-kim-thanh-nhan-a6a893209', '_blank')}
+            onClick={() => window.open('https://www.linkedin.com/in/nhan-nguyen-a6a893209/', '_blank')}
           >
             <ExternalLink className="w-5 h-5 mr-2" />
             LinkedIn
